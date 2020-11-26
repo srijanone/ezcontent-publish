c7fbace08ed7817b1620fc057d1a7d8f22ca2558
saurabh-2k17 <saurabhpsit2k17@gmail.com>
2020-11-26 12:54:35 +0530

    EZ-639: Quick Links Menu block on Moderation Dashboard

--- src/Plugin/Block/EzContentUserShortcutBlock.php
+++ src/Plugin/Block/EzContentUserShortcutBlock.php
@@ -4,11 +4,13 @@ namespace Drupal\ezcontent_publish\Plugin\Block;
 
 use Drupal\Core\Url;
 use Drupal\Core\Link;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+
 
 /**
  * Provides a block with a user wise shortcut links.
@@ -20,12 +22,12 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
  */
 class EzContentUserShortcutBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
-  /**
-   * @var \Drupal\Core\Session\AccountInterface
+   /**
+   * @var AccountInterface $account
    */
   protected $account;
 
-  /**
+   /**
    * The entity type manager.
    *
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
@@ -72,77 +74,59 @@ class EzContentUserShortcutBlock extends BlockBase implements ContainerFactoryPl
 
     // Getting current user role.
     $currentUserRoles = $this->account->getRoles();
-    $shortcutObj = [];
-
     if (!in_array('administrator', $currentUserRoles)) {
       $currentUserId = $this->account->id();
       $currentUsername = $this->account->getUsername();
       $currentShortcutset = $this->entityTypeManager
-        ->getStorage('shortcut_set')
-        ->loadByProperties(['id' => 'shortcut_set_' . $currentUsername . $currentUserId]);
-
+      ->getStorage('shortcut_set')
+      ->loadByProperties(['id' => 'shortcut_set_' . $currentUsername . '_' . $currentUserId]);
       // Getting current user shortcut set.
-      $currentuserShortcutsetId = $this->entityTypeManager
+      $shortcutSet = $this->entityTypeManager
         ->getListBuilder('shortcut_set');
-
       if (!$currentShortcutset) {
         // Creating default shortcut set for new user.
-        $set = $currentuserShortcutsetId->getStorage()->create([
-          'id' => 'shortcut_set_' . $currentUsername . $currentUserId,
-          'label' => 'Default Shortcut Set for ' . $currentUsername,
+        $set = $shortcutSet->getStorage()->create([
+          'id' => 'shortcut_set_' . $currentUsername . '_' . $currentUserId,
+          'label' => $this->t('Default Shortcut Set for @currentUsername', ['@currentUsername' =>$currentUsername])
         ]);
         $set->save();
       }
-
-      // Loading current user shortcuts for display.
-      $currentUserShortcuts = $this->entityTypeManager
-        ->getStorage('shortcut')
-        ->loadByProperties(['shortcut_set' => 'shortcut_set_' . $currentUsername . $currentUserId]);
-
-      // Building Add link URL.
-      $addShortcutUrl = Url::fromRoute('ezcontent_publish.shortcut.link_add', ['shortcut_set' => 'shortcut_set_' . $currentUsername . $currentUserId]);
-      $addShortcutLink = Link::fromTextAndUrl(t('Add Shortcut'), $addShortcutUrl);
-      $addShortcutLink = $addShortcutLink->toRenderable();
-
-      // Building Operation link URL.
-      $operationShortcutUrl = Url::fromRoute('ezcontent_publish.shortcut_set.customize_form', ['shortcut_set' => 'shortcut_set_' . $currentUsername . $currentUserId]);
-      $operationShortcutLink = Link::fromTextAndUrl(t('Visit Shortcuts'), $operationShortcutUrl);
-      $operationShortcutLink = $operationShortcutLink->toRenderable();
-
-      foreach ($currentUserShortcuts as $key => $currentUserShortcutObj) {
-        $shortcutTitle = $currentUserShortcutObj->get('title')->getValue()[0]['value'];
-        $shortcutLink = $currentUserShortcutObj->get('link')->getValue()[0]['uri'];
-        $shortcutObj[0][$shortcutTitle] = $shortcutLink;
-      }
-
+      $shortcutObj = $this->getUserShortcutLinks('shortcut_set_' . $currentUsername . '_' . $currentUserId);
+    } else {
+      $shortcutObj = $this->getUserShortcutLinks('default');
     }
-    else {
-      $defaultShortcuts = $this->entityTypeManager
-        ->getStorage('shortcut')
-        ->loadByProperties(['shortcut_set' => 'default']);
-
-      // Building Add link URL.
-      $addShortcutUrl = Url::fromRoute('ezcontent_publish.shortcut.link_add', ['shortcut_set' => 'default']);
-      $addShortcutLink = Link::fromTextAndUrl(t('Add Shortcut'), $addShortcutUrl);
-      $addShortcutLink = $addShortcutLink->toRenderable();
-
-      // Building Operation link URL.
-      $operationShortcutUrl = Url::fromRoute('ezcontent_publish.shortcut_set.customize_form', ['shortcut_set' => 'default']);
-      $operationShortcutLink = Link::fromTextAndUrl(t('Manage Shortcuts'), $operationShortcutUrl);
-      $operationShortcutLink = $operationShortcutLink->toRenderable();
-
-      foreach ($defaultShortcuts as $key => $defaultShortcutObj) {
-        $defaultShortcutTitle = $defaultShortcutObj->get('title')->getValue()[0]['value'];
-        $defaultShortcutLink = $defaultShortcutObj->get('link')->getValue()[0]['uri'];
-        $shortcutObj[0][$defaultShortcutTitle] = $defaultShortcutLink;
-      }
-    }
-    $shortcutObj[1]['add_link'] = $addShortcutLink;
-    $shortcutObj[1]['manage_link'] = $operationShortcutLink;
     return [
-      '#theme' => 'usershortcutblock_template',
+      '#theme' => 'user_shortcut_block_template',
       '#data' => $shortcutObj,
     ];
+  }
+  public function getCacheTags() {
+    return Cache::mergeTags(parent::getCacheTags(), ['ezcontent_user_shortcut_block']);
+  }
+
+  /**
+   * Implements get_usershortcut_links().
+   */
+  function getUserShortcutLinks($currentUserShortcutSet) {
+    // Building Add link URL.
+    $addShortcutUrl = Url::fromRoute('ezcontent_publish.shortcut.link_add', array('shortcut_set' => $currentUserShortcutSet));
+    $addShortcutLink = Link::fromTextAndUrl(t('Add Shortcut'), $addShortcutUrl)->toRenderable();
+    // Building Operation link URL.
+    $operationShortcutUrl = Url::fromRoute('ezcontent_publish.shortcut_set.customize_form', array('shortcut_set' => $currentUserShortcutSet));
+    $operationShortcutLink = Link::fromTextAndUrl(t('Visit Shortcuts'), $operationShortcutUrl)->toRenderable();
+    // Loading current user shortcuts for display.
+    $currentUserShortcuts =  $this->entityTypeManager
+      ->getStorage('shortcut')
+      ->loadByProperties(['shortcut_set' => $currentUserShortcutSet]);
+    foreach ($currentUserShortcuts as $key => $currentUserShortcutObj) {
+      $shortcutTitle = $currentUserShortcutObj->get('title')->value;
+      $shortcutLink =$currentUserShortcutObj->get('link')->first()->getUrl();
+      $shortcutObj['links'][] = Link::fromTextAndUrl($shortcutTitle, $shortcutLink)->toRenderable();
+    }
+    $shortcutObj['operations']['add_link'] = $addShortcutLink;
+    $shortcutObj['operations']['manage_link'] = $operationShortcutLink;
+
+    return $shortcutObj;
   }
 
 }
